910631a17079697c7fd3ad9a1ea3923985dd867d
thunderdev24 <tayyabshahidstudentait@gmail.com>
2025-12-04 20:00:06 +0500

    day1

---
+++ Index.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+namespace Game{
+    int value = 10;
+}
+*/
+typedef string ifra_malik;
+typedef double tayyab;
+typedef bool has;
+int main() {
+    // ifra_malik firstname = "ifra";
+    // has ya = false;
+    // cout << firstname;
+    // cout << ya;
+
+    tayyab number = 20;
+    cout << number;
+    //int value = 20;
+    //value 2;
+
+return 0;
+}
